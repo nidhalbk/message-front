275312b8941510721801bba86688c4a2373bcf7e
Nidhal Ben Khalifa <nidhal.benkhalifa@oyez.fr>
2019-07-19 15:20:50 +0100

    fix bug contact

--- src/Components/Messaging/Contact/Contact.jsx
+++ src/Components/Messaging/Contact/Contact.jsx
@@ -5,15 +5,19 @@ import contactData from '../../../utils/MockData/contatctList';
 
 export default function Contact() {
   return (
-    <>
-      <div className="card-header">
-        <SearchBar />
+    <div className="col-md-4 col-xl-3 chat">
+      <div className="card mb-sm-3 mb-md-0 contacts_card">
+        <div className="card-header">
+          <SearchBar />
+        </div>
+        <div className="card-body contacts_body">
+          <ul className="contacts">
+            {contactData.map((contact) => (
+              <ContactList contact={contact} />
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="card-body contacts_body">
-        <ul className="contacts">
-          {contactData.map((contact) => <ContactList contact={contact} />)}
-        </ul>
-      </div>
-    </>
+    </div>
   );
 }
--- src/Components/Messaging/Messaging.jsx
+++ src/Components/Messaging/Messaging.jsx
@@ -6,11 +6,7 @@ import Convesation from "./conversation";
 export default function Messaging() {
   return (
     <>
-      <div className="col-md-4 col-xl-3 chat">
-        <div className="card mb-sm-3 mb-md-0 contacts_card">
-          <Contact />
-        </div>
-      </div>
+      <Contact />
       <Convesation />
     </>
   );
