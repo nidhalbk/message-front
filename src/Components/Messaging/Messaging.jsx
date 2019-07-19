7ab7327b7ea001b3d8fc53a1abfb79c7c6eb2f7a
Nidhal Ben Khalifa <nidhal.benkhalifa@oyez.fr>
2019-07-19 15:01:44 +0100

    fix bug merge

--- src/Components/Messaging/Messaging.jsx
+++ src/Components/Messaging/Messaging.jsx
@@ -5,13 +5,13 @@ import Convesation from "./conversation";
 
 export default function Messaging() {
   return (
-    <div>
+    <>
       <div className="col-md-4 col-xl-3 chat">
         <div className="card mb-sm-3 mb-md-0 contacts_card">
           <Contact />
         </div>
       </div>
       <Convesation />
-    </div>
+    </>
   );
 }
